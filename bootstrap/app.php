5fa36bb4d884967aedcf9ec1e278a617674947de
Selcuk Kekec <senycorp@googlemail.com>
2016-06-04 01:45:05 +0200

    Bootstrapping will show DebugBar if app is in development mode

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,6 +41,13 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
+/**
+ * DEBUG-BAR
+ */
+if (env('APP_DEBUG')) {
+    $app->register(Barryvdh\Debugbar\LumenServiceProvider::class);
+}
+
 /*
 |--------------------------------------------------------------------------
 | Return The Application
