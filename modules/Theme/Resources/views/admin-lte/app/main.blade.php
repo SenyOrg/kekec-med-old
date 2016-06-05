02b0f7fe6e9caea58d7cf52f4797c8cb843d1e5c
Selcuk Kekec <senycorp@googlemail.com>
2016-06-05 21:47:03 +0200

    AdminLTE implemented in Theme module

---
+++ modules/Theme/Resources/views/admin-lte/app/main.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        @include('theme::admin-lte.app.partials.head-includes')
+    </head>
+    <body class="hold-transition skin-blue sidebar-mini">
+        @include('theme::admin-lte.app.partials.wrapper')
+    </body>
+</html>
---
+++ modules/Theme/Resources/views/admin-lte/app/partials/content-wrapper.blade.php
@@ -0,0 +1,6 @@
+<div class="content-wrapper">
+    @include('theme::admin-lte.app.partials.head-section')
+
+    @include('theme::admin-lte.app.partials.content-section')
+    <!-- /.content -->
+</div>
