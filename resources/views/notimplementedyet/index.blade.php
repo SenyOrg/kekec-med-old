8383f7ad2cef928d065afc30bbd1110d56689eab
Selcuk Kekec <senycorp@googlemail.com>
2016-06-06 06:30:51 +0200

    View folder is clear now

--- resources/views/notimplementedyet/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app.app')
-
-@section('content')
-    <div class="alert alert-info alert-dismissible">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-        <h4><i class="icon fa fa-info"></i> This module is not implemented yet</h4>
-        Development is in progress...
-    </div>
-@endsection
