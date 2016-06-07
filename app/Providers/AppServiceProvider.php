0943b223dc785816a0306b0841b05ee27c1ef195
Selcuk Kekec <senycorp@googlemail.com>
2016-06-07 20:36:25 +0200

    AppServiceProvider is clean now

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,39 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \Form::component('bsText', 'components.form.text', [
-            'name',
-            'label' => 'ReplaceMe',
-            'namespace' => 'formData',
-            'value' => null,
-            'attributes' => [
-                'placeholder' => 'REPLACEME',
-            ]
-        ]);
-
-        \Form::component('bsFile', 'components.form.file', [
-            'name',
-            'label' => 'ReplaceMe',
-            'namespace' => 'formData',
-            'value' => null,
-            'attributes' => [
-                'placeholder' => 'REPLACEME',
-            ]
-        ]);
-
-        \Form::component('bsDate', 'components.form.datemask', [
-            'name',
-            'label' => 'ReplaceMe',
-            'namespace' => 'formData',
-            'value' => null,
-            'attributes' => [
-                'placeholder' => 'REPLACEME',
-            ]
-        ]);
-
-        \Html::macro('activeState', function ($route) {
-            return strpos(\Request::url(), route($route)) !== false ? 'active' : '';
-        });
+        //
     }
 
     /**
