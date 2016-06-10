6b8aa4cf43bb323dbafcfe93e8076a4eb853547f
Selcuk Kekec <senycorp@googlemail.com>
2016-06-11 01:29:34 +0200

    kekec-med -------------------------------------- #4 -------------------------------------- GENERIC ISSUE --------------------------------------
    ---------COMMIT-MESSAGE-START---------
    
    Core Routes
    
    ----------COMMIT-MESSAGE-END----------

---
+++ modules/Core/Http/Controllers/SessionController.php
@@ -0,0 +1,8 @@
+<?php
+namespace KekecMed\Core\Http\Controllers;
+
+
+class SessionController extends AbstractCoreController
+{
+    
+}
--- modules/Core/Http/routes.php
+++ modules/Core/Http/routes.php
@@ -1,5 +1,23 @@
 <?php
 
-/**
- * The core module does not need any routes
- */
+Route::group(['middleware' => ['web', 'auth'], 'prefix' => 'core', 'namespace' => 'KekecMed\Core\Http\Controllers'], function()
+{
+    /**
+     * Session API
+     */
+	Route::get('/session/{key}/{value?}', function($key, $value) {
+        if (isset($value)) {
+            Session::set($key, $value);
+        } else {
+            Session::get($key);
+        }
+    });
+
+    /**
+     * Password check
+     */
+    Route::get('/check/{password?}', function($password) {
+        return response()->json([
+            'result' => ((is_null($password)) ? false : Hash::check($password, \Auth::user()->password))]);
+    });
+});
